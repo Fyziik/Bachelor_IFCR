4b685e7215406243d5696e2cca43f1dfbdd958f0
Andreas Møller Laursen <Fyziikme@gmail.com>
2023-05-01 23:05:43 +0200

    hooked frontend up with backend & database

---
+++ frontend/src/lib/Data.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { getAllAdmins } from "./api/api";
+  let admins
+  onMount(async () => {
+		const res = await getAllAdmins();
+    admins = res.data
+    console.log(admins)
+	});
+  
+</script>
+
+<main>
+  {#if admins}
+    {#each admins as admin}
+      {admin.username}
+    {/each}
+  {:else}
+    <h1>Fetching data</h1>
+  {/if}
+</main>
+
+<style>
+
+</style>
